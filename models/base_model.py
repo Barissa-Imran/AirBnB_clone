302088bcaa1c09bc1447a9e9ed28316c13477573
Patrick Ndolo <ndolopatrick52@gmail.com>
2023-03-09 12:48:46 +0300

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -23,14 +23,15 @@ class BaseModel:
         return f"[{type(self).__name__}] ({self.id}) <{self.__dict__}>"
 
     def save(self):
-        """updates the public instance attribute `updated_at` with the current datetime"""
+        """updates the public instance attribute `updated_at` with
+        the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all keys/values of __dict__ of
+        the instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = type(self).__name__
         my_dict['created_at'] = my_dict['created_at'].isoformat()
         my_dict['updated_at'] = my_dict['updated_at'].isoformat()
         return my_dict
-
